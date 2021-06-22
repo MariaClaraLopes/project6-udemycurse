1482eb1ef2c16a1b0ee17291814d259a2575f666
MariaClaraLopes <mariacllara.lopes@gmail.com>
2021-06-22 14:11:39 -0300

    mariaclara Add time in project

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+//    let eggTimes = ["Soft": 10, "Medium": 20, "Hard": 30]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func eggSelected(_ sender: UIButton) {
+        let egg = sender.titleLabel?.text ?? ""
+        var result = eggTimes[egg] ?? 0
+        print(result)
+        
+        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+            if result > 0 {
+                print("\(result) segundos")
+                result -= 1
+            } else {
+                Timer.invalidate()
+            }
+        }
+    }
 }
+
